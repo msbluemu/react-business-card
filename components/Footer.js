d64d9f8901e3485a5e65a92f428e652ed736ac40
msbluemu <124859009+msbluemu@users.noreply.github.com>
2023-03-29 13:58:41 +1100

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+export default function Footer(){
+    return (
+      <div className="icons">
+         <img className="twitter" src="./square-twitter.svg" />
+         <img className="facebook" src="./square-facebook.svg" />
+         <img className="instagram" src="./square-instagram.svg" />
+         <img className="linkedin" src="./linkedin.svg" />
+         <img className="github" src="./square-github.svg" />
+      </div>
+    )
+}
---
+++ components/Info.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Info(){
+    return (
+      <div className ="info">
+        <img className="avatar" src="./qianyun.jpeg" />
+        <h1 className="name">Qianyun Mu</h1>
+        <h2 className="position">Frontend Developer</h2>
+        <button className="button">Email</button>
+      </div>
+    )
+}
